119d7b3a9324de3d5ef3fe14c00277a9398036ed
Lune <70208937+Flune@users.noreply.github.com>
2022-10-26 00:08:26 +0200

    Update problem9_plot.py

--- problem9_plot.py
+++ problem9_plot.py
@@ -16,16 +16,16 @@ def values(n):#Returns values from file n
         w=np.array(w)
         n=np.array(n)
     return  f, w, n
-    
+
 f, w, n = values("Scan_broad_f_0.1_n_80000")
-plt.plot(w,np.log(n),label="f=0.1")
+plt.plot(w,n/100,label="f=0.1")
 f, w, n = values("Scan_broad_f_0.4_n_80000")
-plt.plot(w,np.log(n),label="f=0.4")
+plt.plot(w,n/100,label="f=0.4")
 f, w, n = values("Scan_broad_f_0.7_n_80000")
-plt.plot(w,np.log(n),label="f=0.7")
+plt.plot(w,n/100,label="f=0.7")
 plt.legend()
 plt.xlabel("frequency (MHz)")
 plt.ylabel("Number of particles")
-plt.title("Particles over frequency in logarithmic scale")
+plt.title("Particles over frequency")
 plt.savefig("Problem9Output/Particles over frequency.pdf")
 plt.show()
